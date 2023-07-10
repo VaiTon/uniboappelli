8356ffb2733118fb0072f517e3eb42c6590514b9
VaiTon <eyadlorenzo@gmail.com>
2023-07-10 17:06:21 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/VaiTon/uniboappelli
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/rs/zerolog v1.29.1
+)
+
+require github.com/goodsign/monday v1.0.1 // indirect
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/gorilla/feeds v1.1.1
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/gorilla/feeds"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	debug := flag.Bool("debug", false, "sets log level to debug")
+	flag.Parse()
+
+	corsi := flag.Args()
+
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
+	for _, corso := range corsi {
+		rssForCorso(corso)
+	}
+}
+
+func rssForCorso(corso string) {
+	log.Info().Str("corso", corso).Msg("generating rss")
+
+	prove, err := getProve(corso)
+
+	log.Info().Str("corso", corso).Int("nProve", len(prove)).Msg("got prove")
+
+	if err != nil {
+		log.Err(err).Str("corso", corso).Msg("Could not get appelli url")
+		return
+	}
+
+	file, err := os.ReadFile("data.json")
+	oldProve := make([]Prova, 0, 10)
+	if err != nil && !os.IsNotExist(err) {
+		log.Error().Msg("Could not read file")
+		os.Exit(1)
+	} else if err == nil {
+		err = json.Unmarshal(file, &oldProve)
+		if err != nil {
+			log.Error().Msg("Could not parse file")
+			os.Exit(1)
+		}
+	}
+
+	appelliUrl := getAppeliUrl(corso)
+
+	feed := &feeds.Feed{
+		Title:       "Notifiche Appelli",
+		Description: "Notifiche Appelli",
+		Author:      &feeds.Author{Name: "Notifiche Appelli", Email: ""},
+		Link:        &feeds.Link{Href: appelliUrl},
+		Created:     time.Now(),
+	}
+
+	feed.Items = make([]*feeds.Item, 0, len(prove))
+
+	log.Debug().Msg("Sorting prove")
+	var proveSort Prove = prove
+	sort.Sort(sort.Reverse(proveSort))
+
+	for _, p := range proveSort {
+		builder := strings.Builder{}
+
+		builder.WriteString(fmt.Sprintf("Data: %s\n", p.DataEOra))
+		builder.WriteString(fmt.Sprintf("Tipo: %s\n", p.Tipo))
+		builder.WriteString(fmt.Sprintf("Docente: %s\n", p.Materia.Docente))
+		builder.WriteString(fmt.Sprintf("Materia: %s\n", p.Materia.Titolo))
+
+		feed.Items = append(feed.Items, &feeds.Item{
+			Title:       p.Materia.Titolo,
+			Link:        &feeds.Link{Href: appelliUrl},
+			Description: builder.String(),
+		})
+	}
+
+	if os.MkdirAll("rss", 0755) != nil {
+		log.Error().Msg("Could not create RSS folder")
+		os.Exit(1)
+	}
+
+	fileName := fmt.Sprintf("rss/%s.rss", corso)
+	atomFile, err := os.Create(fileName)
+	if err != nil {
+		log.Error().Msg("Could not create RSS")
+		os.Exit(1)
+	}
+	defer atomFile.Close()
+
+	err = feed.WriteAtom(atomFile)
+	if err != nil {
+		log.Error().Msg("Could not create RSS")
+		os.Exit(1)
+	}
+}
+
+func diffProve(new []Prova, old []Prova) []Prova {
+	diff := make([]Prova, 0, 1)
+
+	log.Debug().Int("new", len(new)).Int("old", len(old)).Msg("diffing prove")
+
+	oldMap := proveHashMap(old)
+
+	for _, newProva := range new {
+		if _, ok := oldMap[newProva.String()]; !ok {
+			diff = append(diff, newProva)
+		}
+	}
+
+	return diff
+}
